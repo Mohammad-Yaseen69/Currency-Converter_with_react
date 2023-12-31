7c6363723f14eec1e5dc0dbe9c87ca93f7cdb4be
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2023-12-31 21:01:23 +0500

    Adding custom country codes

---
+++ src/Assets/countryCode.js
@@ -0,0 +1,64 @@
+const countryList = {
+    USD: {
+        countryCode: "US",
+        fullForm: "US Dollar",
+    },
+    EUR: {
+        countryCode: "EU",
+        fullForm: "Euro",
+    },
+    JPY: {
+        countryCode: "JP",
+        fullForm: "Japanese Yen",
+    },
+    GBP: {
+        countryCode: "GB",
+        fullForm: "British Pound",
+    },
+    AUD: {
+        countryCode: "AU",
+        fullForm: "Australian Dollar",
+    },
+    CNY: {
+        countryCode: "CN",
+        fullForm: "Chinese Yuan",
+    },
+    CAD: {
+        countryCode: "CA",
+        fullForm: "Canadian Dollar",
+    },
+    SEK: {
+        countryCode: "SE",
+        fullForm: "Swedish Krona",
+    },
+    NZD: {
+        countryCode: "NZ",
+        fullForm: "New Zealand Dollar",
+    },
+    INR: {
+        countryCode: "IN",
+        fullForm: "Indian Rupee",
+    },
+    PKR: {
+        countryCode: "PK",
+        fullForm: "Pakistani Rupee",
+    },
+    AED: {
+        countryCode: "AE",
+        fullForm: "Dubai Dirham",
+    },
+    SAR: {
+        countryCode: "SA",
+        fullForm: "Saudi Riyal",
+    },
+    QAR: {
+        countryCode: "QA",
+        fullForm: "Qatari Rial",
+    },
+    KWD: {
+        countryCode: "KW",
+        fullForm: "Kuwaiti Dinar",
+    },
+};
+
+export default countryList
